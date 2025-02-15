dce34b8d7686d25ef8515485e92c6a9c4a2a9516
shiven singh <singhshiven39@gmail.com>
2025-02-15 12:40:50 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    double num1, num2, result;
+    int num1, num2;
     char operator;
-    scanf("%lf", &num1);
-    scanf(" %c", &operator); 
-    scanf("%lf", &num2);
-    scanf("%lf",&result);
-    switch(operator) {
+
+    if (scanf("%d %d %c", &num1, &num2, &operator) != 3) {
+        printf("Error: Invalid input format.\n");
+        return 1; 
+    }
+    switch (operator) {
         case '+':
-            
-            printf("%.2lf\n",num1+num2);
+            printf("%d\n", num1 + num2);
             break;
         case '-':
-            result = num1 - num2;
-            printf("%.2lf\n", result);
+            printf("%d\n", num1 - num2);
             break;
         case '*':
-            result = num1 * num2;
-            printf("%.2lf\n", result);
+            printf("%d\n", num1 * num2);
             break;
         case '/':
-            result = num1 / num2;
-                printf("%.2lf\n", result);
-     
-        
+            if (num2 == 0) {
+                printf("error\n");
+                return 1;
+            } else {
+                printf("%d\n", num1 / num2);
+            }
+            break;
+        default:
+            printf("error\n");
+            return 1; 
     }
-        
-}
 
+    return 0; 
+}
